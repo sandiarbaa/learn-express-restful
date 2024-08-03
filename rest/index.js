ed14e1e1c0f9680a6a71e520e600fccf719d1964
sandiarbaa <sandiarbaa@gmail.com>
2024-08-03 17:49:56 +0700

    parsing data dari request body dikirim  menggunakan form atau json

--- rest/index.js
+++ rest/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 
+// middleware untuk menangkap data yg dikirim dari form yg method nya POST, karena kalau tidak pakai middleware maka nilainya akan undefined
+app.use(express.json()); // untuk mengirim data berupa json
+app.use(express.urlencoded({ extended: true }));
+// secara default express sudah memiliki method urlencoded
+// default nya data yg diterima dari form itu dapat diterima, tapi undefined, makanya di express di parsing ke yg namanya middleware, dan ini middleware urlencoded({ extended: true })
+
 app.get("/order", (req, res) => {
   res.send("GET order response!");
 });
 
 app.post("/order", (req, res) => {
-  res.send("POST order response!");
+  // console.log(req.body);
+  const { item, qty } = req.body;
+  res.send(`Item: ${item} - Qty: ${qty}`);
 });
 
 app.listen(8080, () => {
